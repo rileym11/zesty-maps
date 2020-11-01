2f1869ecec7fb400451107d10dbf666700a3ad5b
Riley MacDonald <32280362+rileym11@users.noreply.github.com>
2020-10-31 22:57:42 -0400

    feat(Map): prompt user for location

--- src/containers/Map.tsx
+++ src/containers/Map.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   withScriptjs,
   withGoogleMap,
@@ -13,16 +13,49 @@ type MapComponentP = {
   loadingElement: JSX.Element;
   containerElement: JSX.Element;
   mapElement: JSX.Element;
+};
 
-}
+type Coordinates = {
+  latitude: number;
+  longitude: number;
+};
+
+// TODO: complete functionality (WIP)
 
 // Avoid conflicting with es6 "Map"
 export const MapComponent = withScriptjs(
   withGoogleMap(function MapComponent({}: MapComponentP) {
+    const [coordinates, setCoordinates] = useState<Coordinates>();
+
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition(
+        function ({ coords: { latitude, longitude } }) {
+          setCoordinates({ latitude, longitude });
+        },
+        () => {},
+        { timeout: 10000 }
+      );
+    }, []);
+
     return (
       <div>
-        <GoogleMap defaultZoom={5} defaultCenter={{ lat: 45.5017, lng: -73.5673 }}>
-          {/* Markers */}
+        <GoogleMap
+          defaultZoom={5}
+          defaultCenter={{
+            lat: coordinates?.latitude || 45.5017,
+            lng: coordinates?.longitude || -73.5673,
+          }}
+        >
+          {coordinates && (
+            <Marker
+              position={
+                new google.maps.LatLng(
+                  coordinates.latitude,
+                  coordinates.longitude
+                )
+              }
+            />
+          )}
         </GoogleMap>
       </div>
     );

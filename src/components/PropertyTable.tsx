ff6e4f1879a74a0116463cfcb933fab81e4851d0
Riley MacDonald <32280362+rileym11@users.noreply.github.com>
2020-10-31 21:43:17 -0400

    feat: add PropertyTable componenet

---
+++ src/components/PropertyTable.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Table } from 'react-bootstrap';
+import { Property, PropertyContext } from 'context/PropertyContext';
+import { Link } from 'react-router-dom';
+
+export function PropertyTable() {
+  return (
+    <PropertyContext.Consumer>
+      {({ properties }) => (
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+            </tr>
+          </thead>
+          <tbody>
+            {properties.length > 0 &&
+              (properties as Property[]).map((p, i) => {
+                const [lat, lng] = p.coordinates;
+
+                return (
+                  <tr>
+                    <td>{i + 1}</td>
+                    <td>{lat}</td>
+                    <td>{lng}</td>
+                    <td>
+                      <Link to={`/property/${p.propertyId}`}>Details</Link>
+                    </td>
+                  </tr>
+                );
+              })}
+            {properties.length === 0 && (
+              <tr>
+                <td colSpan={4}>No results</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      )}
+    </PropertyContext.Consumer>
+  );
+}
